a52bc66e683f32c0938ccbac7477a95872a5153f
imad-chikh <imadedinchikh@gmail.com>
2022-12-02 15:52:41 +0100

    publish

---
+++ lib/imad.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Imad extends StatelessWidget {
+  const Imad
+({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
