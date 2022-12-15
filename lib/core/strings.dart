d485ad88cd0b3cdae88d31c27744a5f5476c223c
imad-chikh <imadedinchikh@gmail.com>
2022-12-15 10:56:36 +0100

    n

--- lib/core/strings.dart
+++ lib/core/strings.dart
@@ -1,3 +1,4 @@
 class AppString {
   static const  appName = "Demo";
+  
 }
--- lib/presentation/pages/login/login_page.dart
+++ lib/presentation/pages/login/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/core/const.dart';
 
 import '../../../core/strings.dart';
 
@@ -8,86 +9,83 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [
-        //*logo
-        // title
-        Text("$AppString.appName"),
-        // login text
-        Text('Login'),
-        // email text field
-        _emailTextField(),
-        // password text field
-        _passwordTextField(),
-        // login button
-        _loginButton(),
-        // forgot password
-        _forgotPassword(),
-        // sign up
-        _registerButton(),
-        // social media login 
-        Row(
+      body: Padding(
+        padding: const EdgeInsets.all(PaddingS),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            // google
-            _loginWithGoogleButton(),
-            // facebook
-            _loginWithFacebookButton(),
+            //*logo
+            // title
+            Text("$AppString.appName"),
+            // login text
+            Text('Login'),
+            // email text field
+            _emailTextField(),
+            // password text field
+            _passwordTextField(),
+            // login button
+            _loginButton(),
+            // forgot password
+            _forgotPassword(),
+            // sign up
+            _registerButton(),
+            // social media login
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                // google
+                _loginWithGoogleButton(),
+                // facebook
+                _loginWithFacebookButton(),
+              ],
+            ),
+            // terms and conditions
+            _termsAndConditions(),
           ],
         ),
-        // terms and conditions
-        _termsAndConditions(),
-        
-      ],),
+      ),
     );
   }
 }
 
+_emailTextField() => TextField(
+      decoration: InputDecoration(
+        hintText: 'Email',
+        border: OutlineInputBorder(),
+      ),
+    );
 
+_passwordTextField() => TextField(
+      decoration: InputDecoration(
+        hintText: 'Password',
+        border: OutlineInputBorder(),
+      ),
+    );
 
-_emailTextField()=>
-  TextField(
-    decoration: InputDecoration(
-      hintText: 'Email',
-      border: OutlineInputBorder(),
-    ),
-  );
+_loginButton() => ElevatedButton(
+      onPressed: () {},
+      child: Text('Login'), 
+    );
 
+_forgotPassword() => TextButton(
+      onPressed: () {},
+      child: Text('Forgot Password'),
+    );
 
-_passwordTextField()=>TextField(
-  decoration: InputDecoration(
-    hintText: 'Password',
-    border: OutlineInputBorder(),
-  ),
-);
+_registerButton() => TextButton(
+      onPressed: () {},
+      child: Text('Register'),
+    );
 
-_loginButton()=>ElevatedButton(
-  onPressed: (){},
-  child: Text('Login'),
-);
+_loginWithGoogleButton() => ElevatedButton(
+      onPressed: () {},
+      child: Text('Login with Google'),
+    );
 
+_loginWithFacebookButton() => ElevatedButton(
+      onPressed: () {},
+      child: Text('Login with Facebook'),
+    );
 
-_forgotPassword()=>TextButton(
-  onPressed: (){},
-  child: Text('Forgot Password'),
-);
-
-
-_registerButton()=>TextButton(
-  onPressed: (){},
-  child: Text('Register'),
-);
-
-_loginWithGoogleButton()=>ElevatedButton(
-  onPressed: (){},
-  child: Text('Login with Google'),
-);
-
-
-_loginWithFacebookButton()=>ElevatedButton(
-  onPressed: (){},
-  child: Text('Login with Facebook'),
-);
-
-_termsAndConditions()=>TextButton(
-  onPressed: (){},
-  child: Text('Terms and Conditions')
-  );
+_termsAndConditions() =>
+    TextButton(onPressed: () {}, child: Text('Terms and Conditions'));
